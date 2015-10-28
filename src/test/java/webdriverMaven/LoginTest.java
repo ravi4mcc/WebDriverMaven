fd874d7a554ae5a368d766db213a02d397229a7e
server <ravi@karre.com>
2015-10-28 12:03:02 -0700

    Initial commit

---
+++ src/test/java/webdriverMaven/LoginTest.java
@@ -0,0 +1,38 @@
+package webdriverMaven;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest 
+{
+	private static final String SECONDS = null;
+	public static WebDriver driver;
+	
+	
+	public void setUp()
+	{
+		driver = new FirefoxDriver();
+	}
+	
+	@Test
+	public void doLogin()
+	{
+		driver = new FirefoxDriver();
+		driver.get("http://gmail.com");
+		
+		driver.findElement(By.id("Email")).sendKeys("ravi4mcc@gmail.com");
+		//driver.findElement(By.id("Passwd")).sendKeys("Meher");
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	}
+	
+	@AfterSuite
+	public void close()
+	{
+		driver.close();
+	}
+}
