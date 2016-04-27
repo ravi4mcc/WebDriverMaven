54c3475a0b4455c2e636edaa0bc98429fd1ecdaf
ravi4mcc <ravi4mcc@gmail.com>
2016-04-26 23:01:27 -0700

    Update LoginTest.java

--- src/test/java/webdriverMaven/LoginTest.java
+++ src/test/java/webdriverMaven/LoginTest.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.ChromeDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.Test;
 
